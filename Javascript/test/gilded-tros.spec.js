c8f9d439d62e81b1dfdb85d50c4a41ab0febcd7c
Joey Comhaire <joey.comhaire@gmail.com>
2021-07-15 11:51:58 +0200

    added single test case in describe

--- Javascript/test/gilded-tros.spec.js
+++ Javascript/test/gilded-tros.spec.js
@@ -1,9 +1,11 @@
 import { Item } from '../src/item';
 import { GildedTros } from '../src/gilded-tros';
 
-describe('GildedTrosTest', () => {
-  const items = [new Item('foo', 0, 0)];
-  const app = new GildedTros(items);
-  app.updateQuality();
-  expect(app.items[0].name).toEqual('fixme');
+describe('GildedTros', () => {
+  test('Test Item', () => {
+    const items = [new Item('foo', 0, 0)];
+    const app = new GildedTros(items);
+    app.updateQuality();
+    expect(app.items[0].name).toEqual('fixme');
+  });
 });
